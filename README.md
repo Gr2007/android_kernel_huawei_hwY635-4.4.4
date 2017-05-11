0204eb9360f5dae5a4a664e805bacf81f12ce8f4
Gr2007 <gvjohan99@gmail.com>
2017-05-10 22:39:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# android_kernel_huawei_hwY635-4.4.4
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Tue May 9 23:15:32 CDT 2017"
+#define LINUX_COMPILE_BY "johangr2"
+#define LINUX_COMPILE_HOST "LENOVO-ARCH"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
